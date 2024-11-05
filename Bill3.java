ebdb3ac2556382a4d463f94e90a44565c6199fb6
mhnu16 <manoritamar@gmail.com>
2024-11-05 11:55:46 +0200

    Changed the order of names to fit requirements

--- Bill3.java
+++ Bill3.java
@@ -2,9 +2,9 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-		String name1 = args[0];
+		String name3 = args[0];
 		String name2 = args[1];
-		String name3 = args[2];
+		String name1 = args[2];
 		int cost = Integer.parseInt(args[3]);
 
 		double splitCost = Math.ceil((double) cost / 3);
