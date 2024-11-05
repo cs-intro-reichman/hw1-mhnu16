66aa3600e6113e5759ab793b54a79dae86a526eb
mhnu16 <manoritamar@gmail.com>
2024-11-05 17:31:21 +0200

    Whoopsie-Daisy

--- TimeFormat.java
+++ TimeFormat.java
@@ -14,9 +14,7 @@ public class TimeFormat {
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         
 		String period = hours < 12 ? "AM" : "PM";
-		if (hours == 0) {
-		    hours = 12;
-		} else if (hours > 12) {
+		if (hours > 12) {
 		    hours = hours % 12;
 		}
 
